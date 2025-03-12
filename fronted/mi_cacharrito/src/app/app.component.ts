837c82d0b7b9aff7d2a7fb77a439768b04c16c70
Albert Chacon <roronoaalbert2@gmail.com>
2025-03-12 00:11:01 -0500

    ajustando cocitas uwu

--- fronted/mi_cacharrito/src/app/app.component.ts
+++ fronted/mi_cacharrito/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegistroComponent } from "./componentes/usuario/registro/registro/registro.component";
+import { UsuarioComponent } from "./componentes/usuario/usuario/usuario.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegistroComponent],
+  imports: [RouterOutlet, RegistroComponent, UsuarioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
